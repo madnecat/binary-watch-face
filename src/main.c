b3633f22662b87d27022031430181f489ae34ecc
madnecat <tom_the_rock@hotmail.fr>
2015-11-20 15:28:07 +0100

    heure fonctionnelle
    développement primaire terminé

--- src/main.c
+++ src/main.c
@@ -4,12 +4,14 @@
 #define _RADIUS_1 8
 
 //rayon du cercle vide
-#define _RADIUS_0 7
+#define _RADIUS_0 6
 
 //screen size : 144 * 168
 
 // ****************
+// *              *
 // * Déclarations *
+// *              *
 // ****************
 
 //fenêtre principale
@@ -23,13 +25,25 @@ GColor BG_color;
 GColor MN_color;
 GColor HR_color;
 
+//booléen de premier déclenchement du time handler
+bool first_time_handler = true;
+
+//conteneurs de l'heure actuelle
+int hour, minutes;
+
 //centres des différents cercles
 GPoint MN_center_1, MN_center_2, MN_center_4, MN_center_8, MN_center_16, MN_center_32;
 GPoint HR_center_1, HR_center_2, HR_center_4, HR_center_8, HR_center_16;
 
 // **************
+// *            *
 // * Procédures *
+// *            *
 // **************
+
+// *******************
+// * Initialisations *
+// *******************
 
 //initialisation des coordonnées des cercles
 static void init_pos_circles() {
@@ -42,15 +56,26 @@ static void init_pos_circles() {
   
   //coordonnées des points des minutes
   MN_center_1 = GPoint(bounds.size.w / 2, bounds.size.h / 15);
-  MN_center_2 = GPoint((bounds.size.w / 7) * 6, bounds.size.h / 3.25);
-  MN_center_4 = GPoint((bounds.size.w / 7) * 6, (bounds.size.h / 3.25) * 2.25);
+  MN_center_2 = GPoint((bounds.size.w / 9) * 8, bounds.size.h / 3.25);
+  MN_center_4 = GPoint((bounds.size.w / 9) * 8, (bounds.size.h / 3.25) * 2.25);
   MN_center_8 = GPoint(bounds.size.w / 2, bounds.size.h - bounds.size.h / 15);
-  MN_center_16 = GPoint(bounds.size.w / 7, bounds.size.h / 3.25);
-  MN_center_32 = GPoint(bounds.size.w / 7, (bounds.size.h / 3.25) * 2.25);
+  MN_center_16 = GPoint(bounds.size.w / 9, bounds.size.h / 3.25);
+  MN_center_32 = GPoint(bounds.size.w / 9, (bounds.size.h / 3.25) * 2.25);
+  
+  //coordonnées des points des heures
+  HR_center_1 = GPoint(bounds.size.w / 2, bounds.size.h / 4.5);
+  HR_center_2 = GPoint(bounds.size.w - bounds.size.w / 4.5, bounds.size.h / 2);
+  HR_center_4 = GPoint(bounds.size.w / 2, bounds.size.h - bounds.size.h / 4.5);
+  HR_center_8 = GPoint(bounds.size.w / 4.5, bounds.size.h / 2);
+  
 }
 
+// ***********
+// * Dessins *
+// ***********
+
 //procédure de dessin des cercles en état initial
-static void draw_full_circles(Layer *layer, GContext *ctx) {
+static void draw_full_circles(GContext *ctx) {
   
   //application de la couleur des minutes au dessin
   graphics_context_set_fill_color(ctx, MN_color);
@@ -63,14 +88,122 @@ static void draw_full_circles(Layer *layer, GContext *ctx) {
   graphics_fill_circle(ctx, MN_center_16, _RADIUS_1);
   graphics_fill_circle(ctx, MN_center_32, _RADIUS_1);
   
+  //application de la couleur des minutes au dessin
+  graphics_context_set_fill_color(ctx, HR_color);
+  
+  //dessin des points des heures
+  graphics_fill_circle(ctx, HR_center_1, _RADIUS_1);
+  graphics_fill_circle(ctx, HR_center_2, _RADIUS_1);
+  graphics_fill_circle(ctx, HR_center_4, _RADIUS_1);
+  graphics_fill_circle(ctx, HR_center_8, _RADIUS_1);
+  
+}
+
+//procédure de gestion d'un cercle interne précis
+//unit : unité à tester (heure, minute), stud : palier binaire étudié (32 ,16 ,8 , ...)
+static void handle_internal(GContext *ctx, int *unit, int stud, GPoint center) {
+  
+//  APP_LOG(APP_LOG_LEVEL_DEBUG, "unité = %d, stud : %d", *unit, stud);
+  
+  //étude du cas
+  if(*(unit) / stud > 0) {
+    
+    //maj des minutes restantes à afficher
+    *(unit) -= stud; 
+    
+  } else {
+    
+    //dessin du cercle
+    graphics_fill_circle(ctx, center, _RADIUS_0);
+    
+  }
+  
+}
+
+//procédure de dessin des cercles internes pour affichage de l'heure
+static void draw_intern_circles(GContext *ctx) {
+  
+  //application de la couleur de l'arrière plan
+  graphics_context_set_fill_color(ctx, BG_color);
+  
+  //étude des minutes
+  handle_internal(ctx, &minutes, 32, MN_center_32);
+  handle_internal(ctx, &minutes, 16, MN_center_16);
+  handle_internal(ctx, &minutes, 8, MN_center_8);
+  handle_internal(ctx, &minutes, 4, MN_center_4);
+  handle_internal(ctx, &minutes, 2, MN_center_2);
+  handle_internal(ctx, &minutes, 1, MN_center_1);
+  
+  //étude des heures
+  handle_internal(ctx, &hour, 8, HR_center_8);
+  handle_internal(ctx, &hour, 4, HR_center_4);
+  handle_internal(ctx, &hour, 2, HR_center_2);
+  handle_internal(ctx, &hour, 1, HR_center_1);
+  
+}
+
+// ******************************
+// * Mises à jour contextuelles *
+// ******************************
+
+//Procédure de maj de l'heure dans les variables locales
+static void update_local_time() {
+
+  //initialisation de l'heure
+  time_t temp = time(NULL); 
+  
+  //récupération de la structure contenant l'heure
+  struct tm *tick_time = localtime(&temp);
+  
+  //buffer d'écriture
+  static char buffer[] = "00";
+  
+  //écriture de l'heure actuelle dans le buffer
+  strftime(buffer, sizeof("00"), "%I", tick_time);
+  
+  //récupération de l'heure sous forme d'entier
+  hour = atoi(buffer);
+  
+  //écriture des minutes actuelles dans le buffer
+  strftime(buffer, sizeof("00"), "%M", tick_time);
+  
+  //récupération des minutes sous forme d'entier
+  minutes = atoi(buffer);
+  
 }
 
 //fonction de mise à jour du layer de dessin
 void global_layer_update_proc(Layer *layer, GContext *ctx) {
   
-  draw_full_circles(layer, ctx);
+  //en premier, dessin des cercles pleins
+  draw_full_circles(ctx);
+  
+  //dessin des cercles intérieurs pour affichage de l'heure
+  draw_intern_circles(ctx);
+}
+
+//hook appelé lors du changement de temps
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  
+  //Maj de l'heure dans les variables locales
+  update_local_time();
+  
+  //APP_LOG(APP_LOG_LEVEL_DEBUG, "passage");
+  
+  //dessin des cercles en état initial si pas premier handler
+  if(first_time_handler)
+    //passage du booléen à faux
+    first_time_handler = false;
+  
+  else
+    //dessin du layer
+    layer_mark_dirty(s_global_layer); 
   
 }
+
+// *****************************************
+// * Constructeur/destructeur de la fenêtre *
+// *****************************************
 
 //fonction de construction de la fenêtre
 static void main_window_load(Window *window) {
@@ -79,13 +212,21 @@ static void main_window_load(Window *window) {
   window_set_background_color(window, BG_color); 
   
   //dessin des cercles en état initial
-  layer_mark_dirty(s_global_layer); 
+  //layer_mark_dirty(s_global_layer); 
   
 }
 
 //fonction de destruction de la fenêtre
 static void main_window_unload(Window *window) {
+
+  //désallocation des layers
+  layer_destroy(s_global_layer);
+  
 }
+
+// *****************************************
+// * Constructeur/destructeur du programme *
+// *****************************************
 
 //fonction d'initialisation du contexte
 void init(void) {
@@ -117,9 +258,15 @@ void init(void) {
   
   //initialisation de la position des cercles
   init_pos_circles();
-
+  
+  //initialisation de l'heure dans les variables locales
+  update_local_time();
+  
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
+  
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   
 }
 
@@ -130,6 +277,10 @@ void deinit(void) {
   window_destroy(s_main_window);
   
 }
+
+// ************************
+// * Procédure Principale *
+// ************************
 
 //fonction principale
 int main(void) {
