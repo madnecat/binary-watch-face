78ef7ea5641e860a0f5ddce432d9552cb1460f74
Thomas C <tom_the_rock@hotmail.fr>
2015-11-25 17:01:03 +0100

    Date and color circles

--- src/main.c
+++ src/main.c
@@ -24,9 +24,12 @@ static Window *s_main_window;
 static Layer *s_global_layer;
 
 //couleurs de la watchface
-GColor BG_color;
+GColor BG_color_M; //minutes
+GColor BG_color_H; //heures
+GColor BG_color_C; //Centre
 GColor MN_color;
 GColor HR_color;
+GColor TXT_color;
 
 //conteneurs de l'heure actuelle
 int hour, minutes;
@@ -34,8 +37,17 @@ int hour, minutes;
 //incrément de dessin d'animation !! valeur de base -10 pour détection du premier dessin
 int inc = -10;
 
+//variable de police du jour
+static GFont s_date_font;
+
+//layer de la date
+static TextLayer *s_date_layer;
+
 //booléen de choix du type de dessin
 bool deDrawing = false;
+
+//initialisation du layer de BG
+bool initBG = false;
 
 //centres des différents cercles
 GPoint MN_center_1, MN_center_2, MN_center_4, MN_center_8, MN_center_16, MN_center_32;
@@ -93,16 +105,22 @@ static void update_local_time() {
   static char buffer[] = "00";
   
   //écriture de l'heure actuelle dans le buffer
-  strftime(buffer, sizeof("00"), "%I", tick_time);
+  strftime(buffer, sizeof(buffer), "%I", tick_time);
   
   //récupération de l'heure sous forme d'entier
   hour = atoi(buffer);
   
   //écriture des minutes actuelles dans le buffer
-  strftime(buffer, sizeof("00"), "%M", tick_time);
+  strftime(buffer, sizeof(buffer), "%M", tick_time);
   
   //récupération des minutes sous forme d'entier
   minutes = atoi(buffer);
+  
+  //écrit la date dans le buffer, check du format
+  strftime(buffer, sizeof(buffer), "%d", tick_time);
+  
+  // affichage de la date dans le layer
+  text_layer_set_text(s_date_layer, buffer);
   
 }
 
@@ -175,6 +193,28 @@ static void deDrawingHandler() {
 // * Dessins *
 // ***********
 
+//procédure d'initialisation du BG
+static void drawInitialBG(Layer *layer, GContext *ctx) {
+  
+  //récupération des bounds
+  GRect bounds = layer_get_bounds(layer);
+  
+ // APP_LOG(APP_LOG_LEVEL_DEBUG, "init BG : (%d, %d) : %d", grect_center_point(&bounds).x,grect_center_point(&bounds).y,bounds.size.w);
+  
+  //set de couleur BG Heures
+  graphics_context_set_fill_color(ctx, BG_color_H);
+  
+  //dessin du cercle des heures
+  graphics_fill_circle(ctx, grect_center_point(&bounds), MN_center_2.x - grect_center_point(&bounds).x - _RADIUS_1 / 4);
+  
+    //set de couleur BG centre
+  graphics_context_set_fill_color(ctx, BG_color_C);
+  
+  //dessin du cercle central
+  graphics_fill_circle(ctx, grect_center_point(&bounds), HR_center_2.x - grect_center_point(&bounds).x - 2 * _RADIUS_0);
+  
+}
+
 //procédure de dessin des cercles en état initial
 static void draw_full_circles(GContext *ctx) {
   
@@ -202,7 +242,7 @@ static void draw_full_circles(GContext *ctx) {
 
 //procédure de gestion d'un cercle interne précis
 //unit : unité à tester (heure, minute), stud : palier binaire étudié (32 ,16 ,8 , ...)
-static void handle_internal(GContext *ctx, int *unit, int stud, GPoint center) {
+static void handle_internal(GContext *ctx, int *unit, int stud, GPoint center, GColor color) {
   
 //  APP_LOG(APP_LOG_LEVEL_DEBUG, "unité = %d, stud : %d", *unit, stud);
   
@@ -213,6 +253,9 @@ static void handle_internal(GContext *ctx, int *unit, int stud, GPoint center) {
     *(unit) -= stud; 
     
   } else {
+    
+    //set la couleur du cercle
+    graphics_context_set_fill_color(ctx, color);
     
     //dessin du cercle
     graphics_fill_circle(ctx, center, _RADIUS_0);
@@ -225,21 +268,21 @@ static void handle_internal(GContext *ctx, int *unit, int stud, GPoint center) {
 static void draw_intern_circles(GContext *ctx) {
   
   //application de la couleur de l'arrière plan
-  graphics_context_set_fill_color(ctx, BG_color);
+  graphics_context_set_fill_color(ctx, BG_color_M);
   
   //étude des minutes
-  handle_internal(ctx, &minutes, 32, MN_center_32);
-  handle_internal(ctx, &minutes, 16, MN_center_16);
-  handle_internal(ctx, &minutes, 8, MN_center_8);
-  handle_internal(ctx, &minutes, 4, MN_center_4);
-  handle_internal(ctx, &minutes, 2, MN_center_2);
-  handle_internal(ctx, &minutes, 1, MN_center_1);
+  handle_internal(ctx, &minutes, 32, MN_center_32, BG_color_M);
+  handle_internal(ctx, &minutes, 16, MN_center_16, BG_color_M);
+  handle_internal(ctx, &minutes, 8, MN_center_8, BG_color_M);
+  handle_internal(ctx, &minutes, 4, MN_center_4, BG_color_M);
+  handle_internal(ctx, &minutes, 2, MN_center_2, BG_color_M);
+  handle_internal(ctx, &minutes, 1, MN_center_1, BG_color_M);
   
   //étude des heures
-  handle_internal(ctx, &hour, 8, HR_center_8);
-  handle_internal(ctx, &hour, 4, HR_center_4);
-  handle_internal(ctx, &hour, 2, HR_center_2);
-  handle_internal(ctx, &hour, 1, HR_center_1);
+  handle_internal(ctx, &hour, 8, HR_center_8, BG_color_H);
+  handle_internal(ctx, &hour, 4, HR_center_4, BG_color_H);
+  handle_internal(ctx, &hour, 2, HR_center_2, BG_color_H);
+  handle_internal(ctx, &hour, 1, HR_center_1, BG_color_H);
   
 }
 
@@ -249,7 +292,10 @@ static void draw_intern_circles(GContext *ctx) {
 
 //fonction de mise à jour du layer de dessin
 void global_layer_update_proc(Layer *layer, GContext *ctx) {
-  
+    
+  //dessin du BG
+  drawInitialBG(layer, ctx);
+    
   //en premier, dessin des cercles pleins
   draw_full_circles(ctx);
   
@@ -298,41 +344,7 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 static void main_window_load(Window *window) {
   
   //applique la couleur d'arrière plan
-  window_set_background_color(window, BG_color); 
-  
-  //dessin des cercles en état initial
-  //layer_mark_dirty(s_global_layer); 
-  
-}
-
-//fonction de destruction de la fenêtre
-static void main_window_unload(Window *window) {
-
-  //désallocation des layers
-  layer_destroy(s_global_layer);
-  
-}
-
-// *****************************************
-// * Constructeur/destructeur du programme *
-// *****************************************
-
-//fonction d'initialisation du contexte
-void init(void) {
-  
-  //initialisation des couleurs / TODO paramétrer avec la page de config
-  BG_color = GColorBlack;
-  HR_color = GColorCyan;
-  MN_color = GColorRed;
-  
-  // Create main Window element and assign to pointer
-  s_main_window = window_create();
-  
-  // Set handlers to manage the elements inside the Window
-  window_set_window_handlers(s_main_window, (WindowHandlers) {
-    .load = main_window_load,
-    .unload = main_window_unload
-  });
+  window_set_background_color(window, BG_color_M); 
   
   //instanciation du layer
   Layer *root_layer = window_get_root_layer(s_main_window);
@@ -344,6 +356,58 @@ void init(void) {
   s_global_layer = layer_create(GRect(0, 0, bounds.size.w, bounds.size.h));
   layer_set_update_proc(s_global_layer, global_layer_update_proc);
   layer_add_child(root_layer, s_global_layer);
+  
+  //création du layer de date
+  s_date_layer = text_layer_create(GRect(grect_center_point(&bounds).x - 20, grect_center_point(&bounds).y - 10, 40, 20));
+  text_layer_set_background_color(s_date_layer, BG_color_C);
+  text_layer_set_text_color(s_date_layer, TXT_color);
+  
+  // Create GFont
+  s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_EIGHT_BIT_20 ));
+  
+  // Apply to TextLayer
+  text_layer_set_font(s_date_layer, s_date_font);
+  
+  // Improve the layout to be more like a watchface
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(root_layer, text_layer_get_layer(s_date_layer));
+  
+}
+
+//fonction de destruction de la fenêtre
+static void main_window_unload(Window *window) {
+
+  //désallocation des layers
+  layer_destroy(s_global_layer);
+  text_layer_destroy(s_date_layer);
+  
+}
+
+// *****************************************
+// * Constructeur/destructeur du programme *
+// *****************************************
+
+//fonction d'initialisation du contexte
+void init(void) {
+  
+  //initialisation des couleurs / TODO paramétrer avec la page de config
+  BG_color_M = GColorBlack ;
+  BG_color_H = GColorIslamicGreen;
+  BG_color_C = GColorBlack ;
+  HR_color = GColorBlack ;
+  MN_color = GColorIslamicGreen;
+  TXT_color = GColorIslamicGreen;
+  
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
+  
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
   
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
